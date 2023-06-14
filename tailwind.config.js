922f701dd2d2de0da7984d7ebf9dbe6531e31b46
Firmansyah <firman326syah@gmail.com>
2023-06-14 10:34:33 +0700

    Push

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        'birtu': '#09315F',
+        'biru2': '#0B2447',
+        'birmud': '#2C74B3',
+      },
+    },
+  },
+  plugins: [],
+}
+
